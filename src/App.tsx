b3573a1a0eaf9959283c140c29bdaefa7b5d107f
kalim-Asim <asim1306iiit@gmail.com>
2025-03-17 00:31:11 +0530

    keyboard designed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Key } from "./components/Key"
 
 function App() {
   return (
-    <>
-    {[...Array(26)].map((_, i) => (
-      <Key value={'A'+i}></Key>
-    ))}
-    </>
+    <div className="flex justify-center items-end h-screen bg-black p-4">
+      <div className="flex flex-wrap justify-center gap-2">
+        {Array.from({ length: 26 }, (_, i) => (
+          <Key key={i} value={String.fromCharCode(65 + i)} />
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,6 +1,6 @@
 export const Key = ({value}) => {
   return (
-    <button >
+    <button className="bg-blue-500 w-16 h-16 rounded-lg text-2xl cursor-pointer">
       {value}
     </button>
   )
