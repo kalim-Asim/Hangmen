d96afd932ef930aef0e4e89e9226275abdd75d54
kalim-Asim <asim1306iiit@gmail.com>
2025-03-17 00:49:37 +0530

    hooks added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import { Key } from "./components/Key"
+import { useState } from "react";
+import { Key } from "./components/Key";
+
+const Word = "PUSH"; 
 
 function App() {
+  const [disabledKeys, setDisabledKeys] = useState(Array(26).fill(false));
+
+  const handleKeyPress = (letter: string) => {
+    const index = letter.charCodeAt(0) - 65;
+    setDisabledKeys((prev) => {
+      const newState = [...prev];
+      newState[index] = true;
+      return newState;
+    });
+  };
+
   return (
     <div className="flex justify-center items-end h-screen bg-black p-4">
       <div className="flex flex-wrap justify-center gap-2">
-        {Array.from({ length: 26 }, (_, i) => (
-          <Key key={i} value={String.fromCharCode(65 + i)} />
-        ))}
+        {Array.from({ length: 26 }, (_, i) => {
+          const letter = String.fromCharCode(65 + i);
+          return (
+            <Key
+              key={i}
+              value={letter}
+              disable={disabledKeys[i]}
+              onClick={() => handleKeyPress(letter)}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,7 +1,18 @@
-export const Key = ({value}) => {
+interface ButtonProps {
+  value: string;
+  disable: boolean;
+  onClick: () => void
+}
+export const Key = ({ value, disable, onClick }: ButtonProps) => {
   return (
-    <button className="bg-blue-500 w-16 h-16 rounded-lg text-2xl cursor-pointer">
+    <button
+      className={`w-16 h-16 rounded-lg text-2xl ${
+        disable ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 cursor-pointer"
+      }`}
+      disabled={disable}
+      onClick={onClick}
+    >
       {value}
     </button>
-  )
-}
+  );
+};
